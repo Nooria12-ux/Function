b8ca462c1b979b272615032874923993dd6f2475
Nooria <samihamaysoonnooria@gmail.com>
2025-02-21 16:14:06 +0600

    first commit

---
+++ weather condition.py
@@ -0,0 +1,12 @@
+def weatherCondition(s1,s2):
+    print(f"The season is very cold and cozy in {s1}")
+    print(f"The season is Nice in {s2}")
+    
+season1 = "Winter"
+season2 = "Spring"
+
+weatherCondition(season1, season2)
+
+print()
+
+weatherCondition(season2, season1)
---
+++ well wishes.py
@@ -0,0 +1,11 @@
+def wishes(n):
+    print(f"Hello Good Day {n}!")
+    
+    
+name = "RajMoyukh"
+
+wishes(name)
+wishes(name)
+wishes(name)
+wishes(name)
+wishes(name)
